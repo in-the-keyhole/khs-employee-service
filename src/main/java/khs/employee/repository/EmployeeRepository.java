c61ad950124e5fa0f6351eeb8666ce53fc29e28b
Thomas Kendall <thomas.j.kendall@gmail.com>
2015-10-04 21:58:02 -0500

    The beginning of integration of Hystrix in the DAO layer.

--- src/main/java/khs/employee/repository/EmployeeRepository.java
+++ src/main/java/khs/employee/repository/EmployeeRepository.java
@@ -1,12 +1,15 @@
 package khs.employee.repository;
 
+import org.springframework.stereotype.Component;
+
 import khs.employee.model.Employee;
 import khs.service.base.JPABaseDao;
 
-import org.springframework.stereotype.Component;
-
 @Component
-public class EmployeeRepository extends JPABaseDao<Employee,Long> {
+public class EmployeeRepository extends JPABaseDao<Employee, Long> {
 
-	
+	public EmployeeRepository() {
+		super("EmployeeRepository");
+	}
+
 }
