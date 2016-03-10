23264f0702299e184f7c5409ae126c14906b670d
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-03-10 15:22:38 -0600

    moved Dockerfile to root

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into employee(id, firstname, lastname, email) values (1, 'John', 'Doe', 'johndoe@email.com');
 insert into employee(id, firstname, lastname, email) values (2, 'Jane', 'Doe', 'janedoe@email.com');
+insert into employee(id, firstname, lastname, email) values (3, 'Jaime', 'Niswonger', 'jniswonger@keyholesoftware.com');
