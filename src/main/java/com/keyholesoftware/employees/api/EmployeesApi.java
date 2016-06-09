5cf96908b79b0390a944a9f311adbbe8a4038342
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-06-09 16:16:26 -0500

    added service layer

--- src/main/java/com/keyholesoftware/employees/api/EmployeesApi.java
+++ src/main/java/com/keyholesoftware/employees/api/EmployeesApi.java
@@ -10,46 +10,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keyholesoftware.employees.model.Employee;
-import com.keyholesoftware.employees.model.EmployeeRepository;
 
 @RestController
 public class EmployeesApi {
 
 	@Autowired
-	EmployeeRepository repository;
+	EmployeeService employeeService;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/employees")
 	ResponseEntity<Iterable<Employee>> all() {
-		Iterable<Employee> employees = repository.findAll();
+		Iterable<Employee> employees = employeeService.all();
 		return new ResponseEntity<Iterable<Employee>>(employees, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/employees/{id}")
 	ResponseEntity<Employee> one(@PathVariable("id") Long id) {
-		Employee employee = repository.findOne(id);
+		Employee employee = employeeService.one(id);
 		return employee != null ? new ResponseEntity<Employee>(employee, HttpStatus.OK) : new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/employees")
 	ResponseEntity<Employee> create(@RequestBody Employee Employee) {
-		Employee newEmployee = repository.save(Employee);
+		Employee newEmployee = employeeService.save(Employee);
 		return new ResponseEntity<Employee>(newEmployee, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.PUT, value = "/employees/{id}")
 	ResponseEntity<Employee> update(@PathVariable Long id, @RequestBody Employee employee) {
-		Employee originalEmployee = repository.findOne(id);
+		Employee originalEmployee = employeeService.one(id);
 		originalEmployee.setFirstname(employee.getFirstname());
 		originalEmployee.setLastname(employee.getLastname());
 		originalEmployee.setEmail(employee.getEmail());
 
-		Employee savedEmployee = repository.save(originalEmployee);
+		Employee savedEmployee = employeeService.save(originalEmployee);
 
 		return new ResponseEntity<Employee>(savedEmployee, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/employees/{id}")
 	void update(@PathVariable Long id) {
-		repository.delete(id);
+		employeeService.delete(id);
 	}
 }
