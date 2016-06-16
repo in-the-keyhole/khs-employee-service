54cff739d7ad6b07fec9d684728029ccba40ccf1
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-06-16 16:01:43 -0500

    added HATEOAS via spring-data-rest

--- src/main/java/com/keyholesoftware/employees/model/EmployeeRepository.java
+++ src/main/java/com/keyholesoftware/employees/model/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.keyholesoftware.employees.model;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author Jaime Niswonger
  */
+@RepositoryRestResource
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
 }
