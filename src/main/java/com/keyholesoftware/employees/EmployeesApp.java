34e465ace49715a3c59523b63dbb2c3a7f774964
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-06-27 14:43:40 -0500

    removed unneeded extension

--- src/main/java/com/keyholesoftware/employees/EmployeesApp.java
+++ src/main/java/com/keyholesoftware/employees/EmployeesApp.java
@@ -3,14 +3,13 @@ package com.keyholesoftware.employees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Jaime Niswonger
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EmployeesApp extends WebMvcConfigurerAdapter {
+public class EmployeesApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApp.class, args);
