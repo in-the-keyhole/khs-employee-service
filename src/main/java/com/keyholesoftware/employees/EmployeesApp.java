cb01633559789b92cba72556b1b663d4c8e7ad8d
Jaime Niswonger <jniswonger@keyholesoftware.com>
2016-11-04 10:54:26 -0500

    reset name bug for demo

--- src/main/java/com/keyholesoftware/employees/EmployeesApp.java
+++ src/main/java/com/keyholesoftware/employees/EmployeesApp.java
@@ -4,14 +4,14 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 import khs.trouble.servlet.TroubleServlet;
-
 
 /**
  * @author Jaime Niswonger
@@ -20,6 +20,8 @@ import khs.trouble.servlet.TroubleServlet;
 @EnableDiscoveryClient
 public class EmployeesApp implements ServletContextInitializer {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EmployeesApp.class);
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(EmployeesApp.class, args);
@@ -27,14 +29,10 @@ public class EmployeesApp implements ServletContextInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println( "Employee App #onStartup");
+		LOG.debug("Employee App #onStartup");
 		ServletRegistration servletRegistration = servletContext.addServlet("trouble", TroubleServlet.class);
 		servletRegistration.setInitParameter("token", "abc123");
 		servletRegistration.addMapping("/trouble");
 		servletRegistration.addMapping("/trouble/*");
-		
-		
 	}
-
 }
-
