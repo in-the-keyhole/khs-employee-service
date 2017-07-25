8c4b7d242e512c42e3a5957f19286cbf24d0305a
Jaime Niswonger <jniswonger@keyholesoftware.com>
2017-07-25 14:28:33 -0500

    data migration fix

---
+++ src/main/resources/db/migration/V2__Employee.sql
@@ -0,0 +1,10 @@
+DELETE FROM employee;
+
+INSERT INTO employee(id, firstname, lastname, email) VALUES (1, 'John', 'Doe', 'johndoe@email.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (2, 'Jane', 'Doe', 'janedoe@email.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (3, 'David', 'Pitt', 'dpitt@keyholesoftware.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (4, 'Jaime', 'Niswonger', 'jniswonger@keyholesoftware.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (5, 'Walt', 'Disney', 'wdisney@disney.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (6, 'Mickey', 'Mouse', 'mmouse@disney.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (7, 'Minnie', 'Mouse', 'mmouse2@disney.com');
+INSERT INTO employee(id, firstname, lastname, email) VALUES (8, 'Donald', 'Duck', 'dduck@disney.com');
